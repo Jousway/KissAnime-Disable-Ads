4809a2bc8a9441127f33923e57c672d9156dfc8d
Jousway <alexander_goudjeb@hotmail.com>
2015-04-06 18:08:12 +0200

    Update KissAnimeRemoveAds.user.js
    lets hide more stuff, on more Kiss sites, and add a easy update to check if I updated the code rip, KissAnime gon hate me rip

--- KissAnimeRemoveAds.user.js
+++ KissAnimeRemoveAds.user.js
@@ -3,19 +3,48 @@
 // @namespace   https://github.com/Jousway/KissAnime-Disable-Ads
 // @description Disable anoying KissAnime Ads
 // @include     http://kissanime.com/*
-// @version     1.1
+// @include     http://kissmanga.com/*
+// @include     http://kisscartoon.me/*
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
-var divsToHide = document.getElementsByClassName("divCloseBut");
-
-for(var d = 0; d < divsToHide.length; d++) {
-    divsToHide[d].style.visibility="hidden";
-}
-
-for(var i = 0; i < 200; i++) {
-    ifdef = document.getElementById("adsIfrme"+i);
-    if (ifdef) {
-        ifdef.parentNode.removeChild(ifdef);
+function adrm(div) {
+    var rmad = document.getElementById(div);
+    if (rmad) {
+        rmad.parentNode.removeChild(rmad);
     }
 }
+
+function replac(clas,sor,rel) {
+    var cnt = document.getElementsByClassName(clas);
+    for(var o = 0; o < cnt.length; o++) {
+       var rep = cnt[o].innerHTML;
+       var res = rep.replace(sor,rel);
+       cnt[o].innerHTML = res;
+    }
+}
+
+replac("barTitle","\nRemove ads","KissAnime Disable Ads 1.2");
+replac("title","Remove Ads","Get Script");
+$("a[href='/Message/GoPremium']").attr('href', 'https://github.com/Jousway/KissAnime-Disable-Ads/raw/master/KissAnimeRemoveAds.user.js')
+
+var adbtn = document.getElementsByClassName("divCloseBut");
+
+for(var d = 0; d < adbtn.length; d++) {
+    adbtn[d].style.visibility="hidden";
+}
+
+for(var i = 0; i < 50; i++) {
+    adrm("adsIfrme"+i);
+    adrm("divAds"+i);
+}
+
+adrm("divFloatLeft");
+adrm("divFloatRight");
+adrm("divAds");
+
+
+
+
+
